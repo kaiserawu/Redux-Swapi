862a207aee6348aa974d000c4b72cd08a09607cc
kaiserawu <kaiser.a.wu@gmail.com>
2019-04-16 20:06:08 -0700

    Connect actions to view components, check for bugs

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,6 @@ export const getCharacters = () => dispatch => {
     dispatch({ type: FETCHING });
     axios
       .get(`https://swapi.co/api/people/`)
-      .then(res => dispatch({ type: SUCCESS, payload: res.data }))
+      .then(res => dispatch({ type: SUCCESS, payload: res.data.results }))
       .catch(err => dispatch({ type: FAILURE, payload: err }))
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -11,11 +12,15 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <p>Hey! I'm loadin' here!</p>
+      )
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +32,16 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    isFetching: state.charsReducer.fetching
+  }
+}
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
-    /* action creators go here */
+    getCharacters /* action creators go here */
   }
 )(CharacterListView);
